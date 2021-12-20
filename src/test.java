290845085180afd775c4b3eccd654ebb53fd512b
faker <test@runoob.com>
2021-12-20 19:38:31 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.util.List;
+import javax.swing.*;
+class test                                                                         {
+
+    public static void main(String[] args) {
+           int [] in = {1,2,3};
+        System.out.println(in[1]|in[2]);
+    }
+
+}
